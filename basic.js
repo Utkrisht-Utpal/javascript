02a8f26381c94e66843fa037cfe1cbf684faa1eb
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-03-29 23:46:53 +0530

    This is the newset file

---
+++ basic.js
@@ -0,0 +1,29 @@
+//Flipkart product page copy
+
+// const product = {
+//     product_name: "Parker Jotter Standard CT Blue Pen",
+//     color: "black",
+//     Star: 4.5,
+//     Ratings: "7,002",
+//     dealofday: (true),
+//     price: "MRP: 270",
+//     discount: "5%",
+//     origPrice: 285,
+// };
+
+// console.log(product)
+
+
+//LinkedIn profile elements copy
+
+const profile = {
+    Name: "Utkrisht Utpal",
+    verified: true,
+    headline: "Full Stack developer | CU'28 | Tech Enthusiast | Photographer | Geek",
+    location: "Muzzaffarpur, Bihar, India",
+    experience: "Brainstomers club",
+    connections: (true)
+};
+console.log(profile);
+console.log(typeof profile.verified);
+console.log(typeof profile["connections"]);
